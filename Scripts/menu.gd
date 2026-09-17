f05a4cfbfd2da59148c334bc80ae114fc590f7a3
ErickStrigga <erickperezh099@gmail.com>
2026-09-16 23:01:33 -0400

    Arreglando el menu y agregando el sonido de fondo

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@onready var click_sound = $"Click sound"
+@onready var bg_sound = $"BgSound"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	bg_sound.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,12 +13,18 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_play_button_up() -> void:
-	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+func _on_start_pressed() -> void:
+	click_sound.play()
+	await  get_tree().create_timer(0.3).timeout
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
-func _on_options_button_up() -> void:
-	pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+	click_sound.play()
+	await  get_tree().create_timer(0.3).timeout
 
 
-func _on_exit_button_up() -> void:
+func _on_exit_pressed() -> void:
+	click_sound.play()
+	await  get_tree().create_timer(0.3).timeout
 	get_tree().quit()
